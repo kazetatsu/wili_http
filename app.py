d9ca311512e6a20a8e47e0241edd0d8416e0130b
MaruKazeMaru <kaze47111@gmail.com>
2023-09-24 15:31:47 +0900

    update sock_client.py app.py & add shebang SPDX

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2023 ShinagwaKazemaru
+# SPDX-License-Identifier: MIT License
+
 from flask import Flask, g
 from sock_client import SockClient
+import numpy as np
 
 app = Flask(__name__)
 sock_cli:SockClient=None
@@ -14,7 +20,32 @@ def sock_test(s:str):
     if res is None:
         return "<p>だめ</p>"
     else:
-        return res
+        return 'res = ' + res.decode()
+
+@app.route("/tr_prob/pretty")
+def tr_prob_pretty():
+    res = sock_cli.get_tr_prob()
+    if res is None:
+        return "<p>だめ</p>"
+    else:
+        return str(np.array(res[1]).reshape((res[0], res[0])))
+    
+
+@app.route('/tr_prob')
+def tr_prob():
+    res = sock_cli.get_tr_prob()
+    if res is None:
+        return "<p>だめ</p>"
+    else:
+        n = res[0]
+        if n == 0: return '<p>error in wili_bridge</p>'
+        tr_prob = res[1]
+        ret = []
+        for i in range(n):
+            ss = [str(a) for a in tr_prob[i * n : (i + 1) * n]]
+            ret.append(','.join(ss))
+        return '\n'.join(ret)
+
 
 if __name__ == "__main__":
     sock_cli = SockClient()
--- sock_client.py
+++ sock_client.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2023 ShinagwaKazemaru
+# SPDX-License-Identifier: MIT License
+
 import socket
 import threading
 import time
+import struct
+
+req_tr_prob = int.to_bytes(0, 1, 'little', signed=False)
 
 def socket_send_request(request:str, sock_file_path="/tmp/sock.sock", timeout=5.0) -> str | None:
     sock = socket.socket( \
@@ -46,17 +54,30 @@ class SockClient:
     def try_connect(self, sock_file_path:str) -> bool:
         try:
             self.sock.connect(sock_file_path)
+            print('connected socket_bridge !')
             return True
         except ConnectionRefusedError: return False
         except FileNotFoundError: return False
 
-    def send_request(self, req:str) -> str | None:
+    def send_request(self, req:bytes) -> bytes | None:
         if not self.is_conected:
             self.is_conected = self.try_connect(self.sock_file_path)
         if not self.is_conected: return None
 
         try:
-            self.sock.send(req.encode())
-            return self.sock.recv(1024).decode()
+            self.sock.send(req)
+            return self.sock.recv(1024)
         except:
             return None
+
+    def get_tr_prob(self) -> tuple | None:
+        res = self.send_request(req_tr_prob)
+        if res is None: return None
+        # motion num
+        n = res[0]
+        if n == 0: return (0,)
+        fmt = 'f' * (n * n)
+        try:
+            tr_prob = struct.unpack('<' + fmt, res[1:])
+        except ValueError: return None
+        else: return (n, list(tr_prob))
